3cfd48d50b045496d8728dd632fdd8210ea69c4f
gregorsternat <gregor.sternat@epitech.eu>
2025-05-05 17:50:18 +0200

    feat(main): implement scheduler and graceful shutdown mechanism

--- goapp/cmd/myapp/main.go
+++ goapp/cmd/myapp/main.go
@@ -1,31 +1,73 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-    "github.com/joho/godotenv"
-    "src/internal/client"
-    "src/internal/service"
+	"github.com/joho/godotenv"
+	"src/internal/client"
+	"src/internal/service"
 )
 
 func main() {
-    if err := godotenv.Load(); err != nil {
-        log.Println("Warning: .env file not found")
-    }
+	fmt.Println("Esport Oracle starting...")
 
-    apiToken := os.Getenv("TOKEN")
-    if apiToken == "" {
-        log.Println("Warning: API token not set in environment variables")
-		os.Exit(1)
-    }
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: .env file not found")
+	}
 
-    pandaClient := client.NewPandaScoreClient(apiToken)
-    matchService := service.NewMatchService(pandaClient)
+	pandaToken := getEnvOrExit("PANDASCORE_API_TOKEN", "API token for PandaScore is required")
+	rpcURL := getEnvOrExit("ETHEREUM_RPC_URL", "Ethereum RPC URL is required")
+	contractAddr := getEnvOrExit("CONTRACT_ADDRESS", "Contract address is required")
+	privateKey := getEnvOrExit("PRIVATE_KEY", "Private key is required")
 
-    if err := matchService.FetchAndSaveMatches(); err != nil {
-        fmt.Printf("Error: %v\n", err)
-        os.Exit(1)
-    }
+	cronSchedule := os.Getenv("CRON_SCHEDULE")
+	if cronSchedule == "" {
+		cronSchedule = "*/15 * * * *"
+	}
+
+	pandaClient := client.NewPandaScoreClient(pandaToken)
+
+	ethereumClient, err := client.NewEthereumClient(rpcURL, contractAddr, privateKey)
+	if err != nil {
+		log.Fatalf("client.NewEthereumClient(rpcURL, contractAddr, privateKey): %w", err)
+	}
+
+	matchService := service.NewMatchService(pandaClient, ethereumClient)
+
+	fmt.Println("Running initial update")
+	if err := matchService.RunOnce(); err != nil {
+		log.Printf("Warning: matchService.RunOnce(): %w", err)
+	}
+
+	fmt.Println("Starting scheduler...")
+	if err := matchService.StartScheduler(cronSchedule); err != nil {
+		log.Fatalf("matchService.StartScheduler(cronSchedule): %w", err)
+	}
+
+	fmt.Println("Service running !")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	fmt.Println("Shutdown signal received stopping...")
+	matchService.StopScheduler()
+
+	fmt.Println("Waiting...")
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Esport Oracle stopped")
+}
+
+func getEnvOrExit(key, errorMsg string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		log.Fatalf("os.Getenv(%s): %s", key, errorMsg)
+	}
+	return value
 }
