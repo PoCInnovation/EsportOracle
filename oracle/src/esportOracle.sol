65197833f6a12807875af6971b96d70abd180f51
Jules Lordet <jules.lordet@epitech.eu>
2025-05-06 17:30:43 +0200

    🚨 Fix: dynamic structure

--- oracle/src/esportOracle.sol
+++ oracle/src/esportOracle.sol
@@ -57,9 +57,29 @@ contract EsportOracle {
      */
     function addNewMatch(Match[] memory newMatch) external {
         for (uint8 i = 0; i < newMatch.length; i++) {
-            _matchMapping[newMatch[i]._id] = newMatch[i];
+            uint256 matchId = newMatch[i]._id;
+
+            _matchMapping[matchId]._id = matchId;
+            _matchMapping[matchId]._winnerId = newMatch[i]._winnerId;
+            _matchMapping[matchId]._beginAt = newMatch[i]._beginAt;
+            
+            for (uint256 j = 0; j < newMatch[i]._opponents.length; j++) {
+                Opponents memory opponent = newMatch[i]._opponents[j];
+                _matchMapping[matchId]._opponents.push(opponent);
+            }
+
+            for (uint256 j = 0; j < newMatch[i]._game.length; j++) {
+                Games memory game = newMatch[i]._game[j];
+                _matchMapping[matchId]._game.push(game);
+            }
+            
+            for (uint256 j = 0; j < newMatch[i]._result.length; j++) {
+                Result memory result = newMatch[i]._result[j];
+                _matchMapping[matchId]._result.push(result);
+            }
         }
     }
+    
     /**
      * @notice returns the match by id
      * @param matchId The id of the match
