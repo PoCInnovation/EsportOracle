da27a76a632c94cdd4417c2eb3c318ad86f2fba4
Aaron <aaron.aniambossou@epitech.eu>
2025-09-10 19:04:47 +0200

    feat: add new import from multi-select.css

--- front/src/main.ts
+++ front/src/main.ts
@@ -10,6 +10,7 @@ import App from './App.vue'
 import router from './router'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import Ripple from 'primevue/ripple'
+import './styles/multiselect-custom.css'
 
 const blackOrangePreset = definePreset(Aura, {
   semantic: {
